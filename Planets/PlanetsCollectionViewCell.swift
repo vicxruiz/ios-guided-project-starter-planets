8a2e7c00d4435ff25598265000cd35d560b7ff12
victor <ruizdvictor@gmail.com>
2019-05-02 13:15:55 -0400

    Added collection view controller and cell

---
+++ Planets/PlanetsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PlanetsCollectionViewCell.swift
+//  Planets
+//
+//  Created by Victor  on 5/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlanetsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var planetLabel: UILabel!
+    @IBOutlet weak var planetImageView: UIImageView!
+    
+    
+}
---
+++ Planets/PlanetsCollectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlanetsCollectionViewController.swift
+//  Planets
+//
+//  Created by Victor  on 5/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlanetsCollectionViewController: UICollectionViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    
+}
+
+
